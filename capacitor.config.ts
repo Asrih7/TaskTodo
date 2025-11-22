4645aaab6b30e94f30d681d66a4164b9bc06851b
Soufiane Asrih <asrihsoufiane@gmail.com>
2025-11-22 14:27:09 +0100

    generates assests android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.app.taskstodo',
-  appName: 'tasks-todo',
+  appName: 'Tasks Todo',
   webDir: 'build',
   plugins: {
     SplashScreen: {
